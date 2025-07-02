a3a8fbcbe840f75423fc465f258a65a067e4ac35
nxstray <afwanapriansyah@gmail.com>
2025-07-02 21:42:35 +0700

    adding new + updating both DTO: done

---
+++ src/main/java/com/webpi/backend/dto/AnovaDTO.java
@@ -0,0 +1,17 @@
+package com.webpi.backend.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class AnovaDTO {
+    private String namaKasus;
+    private String namaVariabelDependen;
+    private String ho;
+    private String ha;
+    private Double alpha;
+    private String inputMethod;
+
+    private List<String> namaGrup;
+    private List<List<Double>> nilaiGrup;
+}
---
+++ src/main/java/com/webpi/backend/dto/KorelasiDTO.java
@@ -0,0 +1,22 @@
+package com.webpi.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class KorelasiDTO {
+    private String namaKasus;
+    private String namaVarX;
+    private String namaVarY;
+    private String ho;
+    private String ha;
+    private Double alpha;
+    private String inputMethod;
+
+    @JsonProperty("xValues")
+    private List<Double> xValues;
+
+    @JsonProperty("yValues")
+    private List<Double> yValues;
+}
