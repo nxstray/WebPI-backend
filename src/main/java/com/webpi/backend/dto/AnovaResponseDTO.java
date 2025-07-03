c225edd79d26e65c5765f76b3b33ea8d7b46fd12
nxstray <afwanapriansyah@gmail.com>
2025-07-04 05:48:38 +0700

    adding new DTO for GET: done

---
+++ src/main/java/com/webpi/backend/dto/AnovaResponseDTO.java
@@ -0,0 +1,23 @@
+package com.webpi.backend.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AnovaResponseDTO {
+    private Long idAnova;
+    private String namaKasus;
+    private String namaVarY;
+    private String ho;
+    private String ha;
+    private Double alpha;
+    private String inputMethod; 
+    private List<GrupDTO> grups;
+
+    @Data
+    public static class GrupDTO {
+        private String grup;
+        private Double nilai;
+    }
+}
