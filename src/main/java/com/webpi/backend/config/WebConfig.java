2ee1a64e55db6b1511b45774f42767b83bbd721e
nxstray <afwanapriansyah@gmail.com>
2025-07-04 23:47:28 +0700

    url problems: solved

--- src/main/java/com/webpi/backend/config/WebConfig.java
+++ src/main/java/com/webpi/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://webpi-frontend.vercel.app")
+                        .allowedOriginPatterns("https://*.vercel.app")
                         .allowedMethods("*");
             }
         };
