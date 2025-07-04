da54d8a1833dd2c96a9b30dc1acb4dcc8c2aa6ef
nxstray <afwanapriansyah@gmail.com>
2025-07-05 00:32:06 +0700

    404 problems

--- src/main/java/com/webpi/backend/config/WebConfig.java
+++ src/main/java/com/webpi/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("https://*.vercel.app")
+                        .allowedOriginPatterns("https://webpi-frontend-hut7tdapl-nxstrays-projects.vercel.app/")
                         .allowedMethods("*");
             }
         };
